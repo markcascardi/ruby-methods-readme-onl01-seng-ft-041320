5624d074f9321d4ce2dcd31eb56ac05afc24360c
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 04:01:22 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,6 @@
 # Use this space to code along with the readme
 
-ef say_hello_world_ten_times
+def say_hello_world_ten_times
   phrase = "Hello World!"
   puts phrase
   puts phrase
@@ -13,3 +13,7 @@ ef say_hello_world_ten_times
   puts phrase
   puts phrase
 end
+
+def  puts "Hello World"
+end
+greeting
